4f699aa4a14bdb6472b3daf44d7b9c7c3f1d7960
FIRAT AKSU <aksu_fr@yahoo.es>
2018-03-17 11:02:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,12 +62,13 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  var total = total()
   if(cardNumber === undefined){
     return "Sorry, we don't have a credit card on file for you."
   }
   else{
     cart.length = 0
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
   
 }
